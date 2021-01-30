1b23f19d29a0c5284df1552aecf1089d91a9436d
Twelve Chairs Software, LLC <56702533+twelvechairssoftware@users.noreply.github.com>
2021-01-30 11:08:30 -0500

    Update test-vga.asm

--- test-vga.asm
+++ test-vga.asm
@@ -13,9 +13,9 @@
 .model small
 .stack 100h
 .data
-	X 		dw 	?
-	Y 		dw 	?
-	COLOR	db  ?
+	X 	dw 	?
+	Y 	dw 	?
+	COLOR	db  	?
 
 .code
 
@@ -24,32 +24,32 @@ MAXY EQU 200
 
 main:
 	mov ax, 13h
-	int 10h           		;switch to VGA 320x200
+	int 10h           	;switch to VGA 320x200
 
-	mov X, 0				;initialize X
-	mov Y, 0				;initialize Y
-	mov COLOR, 0			;initialize COLOR
+	mov X, 0		;initialize X
+	mov Y, 0		;initialize Y
+	mov COLOR, 0		;initialize COLOR
 
-	jmp plotpixel			;begin plotting
+	jmp plotpixel		;begin plotting
 
 nextline:
-	mov X, 0				;reset X
-	inc COLOR				;next color
+	mov X, 0		;reset X
+	inc COLOR		;next color
 	inc Y               	;next Y-coordinate / line
-	cmp Y, MAXY				;check Y screen overflow
+	cmp Y, MAXY		;check Y screen overflow
 	jne plotpixel
 	jmp keypress        	;done drawing, listen for keystroke
 
 plotpixel:
-	mov ax, Y				;store Y in ax
-	mov bx, X 				;store X in bx
+	mov ax, Y		;store Y in ax
+	mov bx, X 		;store X in bx
 	mov cx, MAXX        	;store 320 in cx
-							;calculate video memory offset as Y*320 + X
+				;calculate video memory offset as Y*320 + X
 	mul cx              	;multiply cx by ax and store in ax
-	add ax, bx     			;add ax to cx and store in ax
+	add ax, bx     		;add ax to cx and store in ax
 	mov di, ax              ;move calculated offset to di
-	mov bx, 0A000h			;set video segment
-	mov es, bx 				;use X as start address
+	mov bx, 0A000h		;set video segment
+	mov es, bx 		;use X as start address
 	mov dl, COLOR
 	mov es:[di], dl         ;write offset/color
 
@@ -67,17 +67,9 @@ keypress:
 exit:
 	mov ah, 00h
 	mov al, 03h
-	int 10h            		;switch to text mode
+	int 10h            	;switch to text mode
 
 	mov ah, 4Ch
 	int 21h               	;exit to DOS
 
 end main
-
-;PROC something_proc
-;ENDP something_proc
-
-;MACRO something x,y
-	;push x
-	;call something_proc
-;ENDM
